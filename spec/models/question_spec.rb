b48e091962bfd4dcce1ac08cfef5eeb87e399228
Rob Bikmansurov <robb@mail.ru>
2020-09-21 16:26:53 +0500

    add spec model question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,23 +1,14 @@
-# (c) goodprogrammer.ru
-
 require 'rails_helper'
 
-# Тестовый сценарий для модели Вопрос
-# Вопрос не содержит функционала (это просто хранилище данных),
-# поэтому все тесты сводятся только к проверке наличия нужных валидаций.
-#
-# Обратите внимание, что работу самих валидаций не надо тестировать (это работа
-# авторов rails). Смысл именно в проверке _наличия_ у модели конкретных валидаций.
 RSpec.describe Question, type: :model do
+  # Проверяем наличие валидации
+  it { should validate_presence_of :level }
+  it { should validate_presence_of :text }
+  # Проверяем, что уровень входит в диапазон
+  it { should validate_inclusion_of(:level).in_range(0..14) }
 
-  context 'validations check' do
-
-    it { should validate_presence_of :text }
-    it { should validate_presence_of :level }
-
-    it { should validate_inclusion_of(:level).in_range(0..14) }
-
-    it { should allow_value(14).for(:level) }
-    it { should_not allow_value(15).for(:level) }
-  end
+  # Проверяем, что модель не разрешает значение 500
+  it { should_not allow_value(500).for(:level) }
+  # Проверяем, разрешает ли наша модель значение 14
+  it { should allow_value(14).for(:level) }
 end
