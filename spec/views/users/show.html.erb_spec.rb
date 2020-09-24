b0040272847d81dcf92d304b61212af96f8ee2df
Rob Bikmansurov <robb@mail.ru>
2020-09-24 11:41:14 +0500

    fix - split in two context

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,30 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  let(:logged_user) { FactoryBot.create(:user, name: 'Иван Иванов') }
-  before(:each) do
-    assign(:user, logged_user)
-    assign(:games, [FactoryBot.build_stubbed(:game),
-                    FactoryBot.build_stubbed(:game)] )
+  context 'unlogged user' do
+    before(:each) do
+      assign(:user, FactoryBot.build_stubbed(:user, name: 'Петр Петров'))
+      assign(:games, FactoryBot.build_stubbed_list(:game, 2))
 
-    render
-  end
-
-  it 'renders name' do
-    expect(rendered).to match('Иван Ивано')
-  end
-
-  it 'render games' do
-    expect(rendered).to have_content('в процессе')
+      render
+    end
+    it 'renders name' do
+      expect(rendered).to match('Петр Петров')
+    end
+    it 'renders games' do
+      expect(rendered).to have_content('в процессе')
+    end
+    it 'not renders change password link' do
+      expect(rendered).not_to have_content('Сменить имя и пароль')
+    end
   end
 
   context 'logged user' do
-    it 'render change password if logged user' do
-      sign_in logged_user
-      @user = logged_user
+    let(:user) { FactoryBot.create(:user, name: 'Иван Иванов') }
+    before(:each) do
+      sign_in user
+      assign(:user, user)
+      assign(:games, FactoryBot.build_stubbed_list(:game, 2))
 
       render
-      expect(rendered).to match('пароль')
+    end
+    it 'renders name' do
+      expect(rendered).to match('Иван Иванов')
+    end
+    it 'renders games' do
+      expect(rendered).to have_content('в процессе')
+    end
+    it 'render change password link' do
+      expect(rendered).to match('Сменить имя и пароль')
     end
   end
 end
